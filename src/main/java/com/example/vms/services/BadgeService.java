061dfa2daaa2a36928013fee0d79710fbc018552
Rania-Mouhib <93470202+Rania-Mouhib@users.noreply.github.com>
2024-01-22 15:57:40 +0100

    more files
    more files

--- src/main/java/com/example/vms/services/BadgeService.java
+++ src/main/java/com/example/vms/services/BadgeService.java
@@ -1,11 +1,13 @@
 package com.example.vms.services;
+import java.util.List;
+import java.util.Optional;
 
 import com.example.vms.models.Badge;
 import com.example.vms.repositories.BadgeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 
 @Service
 public class BadgeService {
@@ -17,4 +19,33 @@ public class BadgeService {
     public List<Badge> findAll() {
         return badgeRepository.findAll();
     }
+    
+    //Save Badge
+  		public void save(Badge badge) {
+  			badgeRepository.save(badge);
+  		}
+  		
+  	//Get Visitor By Id
+  		public Optional<Badge> findById(Long id) {
+  			return badgeRepository.findById(id);
+  		}	
+  		
+  		
+  		public void delete(Long id) {
+  			badgeRepository.deleteById(id);
+  	    }
+  		
+  		public Badge getById(Long id) {
+  	        return badgeRepository.findById(id).orElse(null);
+  	    }
+
+  	    public void update(Badge badge) {
+  	    	badgeRepository.save(badge);
+  	    }
+
 }
+
+
+
+
+
